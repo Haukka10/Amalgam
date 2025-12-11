4371ba39cc4e538407ee75a3802c062997e2f890
Haukka10 <sokoskib@gmail.com>
2025-12-11 23:57:30 +0100

    Add card behavior assets and enable Visual Scripting
    Introduced new card behavior assets (AdHominem, Subversion, Test) and their meta files. Updated CardPrefab to include Visual Scripting components. Linked a behavior graph to the TextCard asset. Moved enums for CardDomain, CardType, SlotType, and Player to global scope in Structures.cs. Added Unity Visual Scripting generated files and project settings to support visual scripting workflows.

--- Assets/Scripts/RitualGame/Structures/Structures.cs
+++ Assets/Scripts/RitualGame/Structures/Structures.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using Unity.VisualScripting;
+
+public enum CardDomain { K, P, M, D } // Domeny kart
+public enum CardType { Normal, Modifier } // Typ karty
+public enum SlotType { BACK, MOD, MID, FRONT, BATTLEFIELD, Valhalla, Graveyard }
+public enum Player { Human, AI }
 
 namespace CardGame.Structures
 {
     public class Structures
     {
-        public enum CardDomain { K, P, M, D } // Domeny kart
-        public enum CardType { Normal, Modifier } // Typ karty
-        public enum SlotType { BACK, MOD, MID, FRONT, BATTLEFIELD, Valhalla, Graveyard}
-        public enum Player { Human, AI }
+
     }
 }
